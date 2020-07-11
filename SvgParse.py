658729e9bc7f410b59b4cd609268714badfcb43c
Tomaslapes <tom@x-com.cz>
2020-07-11 18:06:26 +0200

    Color and stroke width
    Added support for color and variable stroke width

--- SvgParse.py
+++ SvgParse.py
@@ -3,25 +3,39 @@ from matplotlib import pyplot as plt
 
 USE_MILLIMETERS = True
 
-tree = ET.parse("TestFiles/Tomtest3.svg")
+tree = ET.parse("TestFiles/HouseTest.svg")
 root = tree.getroot()
 
-print(type(root))
+#print(type(root))
 
-print(root.attrib)
+#print(root.attrib)
 
-print(root[0].attrib)
+#print(root[0].attrib)
 
-print(type(root[0]))
+#print(type(root[0]))
 
 for child in root:
-    print(child.attrib)
+    print(child.attrib["style"])
     print(child.tag)
 
 print(root[0].attrib['d'])
 #*****Extract additional data **
 Colors = [] #list of string colors
 strokeWidths = [] # list of stroke widths as floats
+
+def rgb_to_hex(rgb):
+    if type(rgb) == str:
+        temp1 = rgb.strip("(")
+        temp1 = rgb.strip(")")
+        templst = temp1.split(",")
+        tempfin_ = []
+        print(templst)
+        for num in templst:
+            tempfin_.append(int(num))
+        rgb_tuple = tuple(tempfin_)
+        print(rgb_tuple)
+        return '%02x%02x%02x' % rgb_tuple
+    return '%02x%02x%02x' % rgb
 
 #*********** Parsing ***********
 print("**************Parsing")
@@ -51,7 +65,7 @@ for child in childLst:
         if USE_MILLIMETERS:
             x=float(coordList[0])/11.811024
             LinesX.append(x)
-            y=float(coordList[1])/11.811024
+            y=float(coordList[1])/11.811024*-1
             LinesY.append(y)
         else:
             x=float(coordList[0])
@@ -64,6 +78,24 @@ for child in childLst:
     lineCollection.append(Lines)
     lineXCollection.append(LinesX)
     lineYCollection.append(LinesY)
+
+    #******* Extract additional info ******
+    style = child["style"]
+    styles = style.split(";")
+    for atr in styles:
+        if "stroke:" in atr:
+            color = atr.split(":")[1]
+            if "rgb" in atr:
+                color = color.strip("rgb(")
+                color = rgb_to_hex(color)
+                color = "#" + color
+            Colors.append(color)
+        if "stroke-width" in atr:
+            pixelsSTR = atr.split(":")[1]
+            strokeWidths.append(float(pixelsSTR.strip("px")))
+    print(Colors)
+    print(strokeWidths)
+        
         
             
 
@@ -94,6 +126,6 @@ print(yCoords)
 #***********Debug Display***********
 print("***********Debug Display***********")
 for i in range(0,len(lineXCollection)):
-    plt.plot(lineXCollection[i],lineYCollection[i])
+    plt.plot(lineXCollection[i],lineYCollection[i],color=Colors[i],linewidth = strokeWidths[i])
 plt.show()
 
